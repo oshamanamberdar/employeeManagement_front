955e45d1234b9c5300b6aef0b26a28924c588404
oshamanamberdar <oshama.namberdar@sbsolutionsnepal.com>
2022-01-06 09:57:52 +0545

    Employee Management simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./employee";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {EmployeeService} from "./employee.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit{
+  employees: Employee[] | undefined;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe((response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+public onOpenModel(employee: Employee, mode: string): void{
+    const button = document.createElement('button');
+    button.type='button';
+    button.style.display='none'
+    button.setAttribute('data-toggle','model');
+    if(mode === 'add') {
+      button.setAttribute('data-target', 'addEmployeeModal')
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', 'addEmployeeModal')
+    }
+  if(mode === 'delete'){
+    button.setAttribute('data-target', 'addEmployeeModal')
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NbButtonModule, NbCardModule, NbLayoutModule, NbThemeModule} from "@nebular/theme";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NbCardModule,
+    NbButtonModule,
+    NbThemeModule.forRoot(),
+    RouterModule.forRoot([]),
+    NbLayoutModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
